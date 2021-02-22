52fb5b76e5045fece3104a05e03ec7056f626257
Luigi <lret@ziggo.nl>
2021-02-22 14:00:33 +0100

    Burger flat view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import { Canvas } from 'react-three-fiber'
+import { OrbitControls, ContactShadows, Environment, Html } from '@react-three/drei'
+
+import { ReactComponent as Logo } from './logo.svg';
+import Burger from './components/burger/Burger'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App" style={{width:"100vw", height:"100vh"}}>
+    <Canvas camera={{ position: [1, 1.8, 3] }}>
+      <spotLight intensity={0.3} position={[5,20,20]} />
+      <ambientLight intensity={0.5} />
+      <Suspense fallback={<Html fullscreen className="App-logo-container"><span><Logo height="40vh" /></span><p>Loading...</p></Html>}>
+        <Burger />
+        <Environment files={"hdr.hdr"}/>
+        <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.6, 0]} opacity={0.25} width={10} height={10} blur={2} far={1}/>
+      </Suspense>
+      <OrbitControls/>
+    </Canvas>
+  </div>
+);
 
 export default App;
